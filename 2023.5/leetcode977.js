0aa765220de66b29f06528129dcdc30fd1d3c753
candyyhguo <candyyhguo@tencent.com>
2023-05-06 08:25:16 +0800

    leetcode977提交

---
+++ 2023.5/leetcode977.js
@@ -0,0 +1,14 @@
+var sortedSquares = function(nums) {
+    const result=[];
+    let k=nums.length-1;i=0;j=nums.length-1;
+    while(i<=j){
+     if(nums[i]*nums[i]<nums[j]*nums[j]){
+            result[k--]=nums[j]*nums[j];
+            j--;
+        }else{
+            result[k--]=nums[i]*nums[i];
+            i++
+        }
+    }
+    return result;
+};
